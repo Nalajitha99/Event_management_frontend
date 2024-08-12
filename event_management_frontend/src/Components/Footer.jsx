c94b6bf6d3d3b4d4dfd4fa4ff7bc0d39182cd2d9
Sanjana_Nalajitha <snalajitha@gmail.com>
2024-08-12 18:16:41 +0530

    did some changes on footer

--- event_management_frontend/src/Components/Footer.jsx
+++ event_management_frontend/src/Components/Footer.jsx
@@ -4,18 +4,24 @@ import { assets } from '../Assets/assets'
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutubeSquare } from "react-icons/fa";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-      <div className="footer-contenrt">
+      <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt=""/>
             <p>EventHub, Sri Lanka's premier and most trusted online ticket partner, serves as the official marketplace providing a secure and safe platform for purchasing tickets to all entertainment events in Sri Lanka.</p>
             <div className="footer-social-icons">
-                <FaFacebookSquare style={{fontSize:"30px",marginRight:"5px"}} />
-                <FaInstagramSquare style={{fontSize:"30px",marginRight:"5px"}}/>
-                <FaSquareXTwitter style={{fontSize:"30px",marginRight:"5px"}} />
+                <FaFacebookSquare style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}} />
+                <FaInstagramSquare style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}}/>
+                <FaSquareXTwitter style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}} />
+                <FaLinkedin style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}} />
+                <FaYoutubeSquare style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}} />
+                <FaWhatsappSquare style={{fontSize:"30px",marginRight:"8px",borderRadius:"100%"}} />
             </div>
         </div>
         <div className="footer-content-center">
