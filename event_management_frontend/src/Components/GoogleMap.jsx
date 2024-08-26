f38e100f6ff5f795f05a6b9bdf177e4b6abaa254
Sanjana_Nalajitha <snalajitha@gmail.com>
2024-08-26 14:07:09 +0530

    Did some changes on google map component

--- event_management_frontend/src/Components/GoogleMap.jsx
+++ event_management_frontend/src/Components/GoogleMap.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
@@ -14,10 +14,30 @@ const center = {
 };
 
 const GoogleMaps = () => {
+
+    const [userLocation, setUserLocation] = useState(null);
+
+    useEffect(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setUserLocation({ lat: latitude, lng: longitude });
+          },
+          (error) => {
+            console.error("Error getting location", error);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    }, []);
+
   return (
-    <LoadScript googleMapsApiKey="YOUR_API_KEY">
+    <LoadScript googleMapsApiKey="AIzaSyB5Z65P9h9gbY-q_t83oKHmMUmj0aPmigg">
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         <Marker position={center} />
+        {userLocation && <Marker position={userLocation} />}
       </GoogleMap>
     </LoadScript>
   );
--- event_management_frontend/src/Pages/Dashboard/Dashboard.jsx
+++ event_management_frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
 import NavBar from '../../Components/NavBar'
 import GoogleMap from '../../Components/GoogleMap'
+import { Button, Grid, TextField, Typography } from '@mui/material'
+import { Search } from '@mui/icons-material'
 
 const Dashboard = () => {
   return (
     <div>
         <NavBar userType="admin"/>
+        <Grid container spacing={0} item xs={8}>
+            <Grid item xs={6}>
+                <div className='mt-5 ml-5'>
+                    <Typography variant='h5'>
+                        See Events Near By
+                    </Typography>
+                    <Typography>
+                        Join to nearby event based on your location
+                    </Typography>
+                </div>
+            </Grid>
+            <Grid item xs={3}>
+                <div className='ml-5 mt-9'>
+                    <TextField
+                        placeholder='Search by area'
+                        variant='outlined'
+                        size='small'
+                    />
+                </div>
+            </Grid>
+            <Grid item xs={2}>
+                <div className='ml-5 mt-9'>
+                    <Button
+                        sx={{width:"60%",}}
+                        placeholder='Search by area'
+                        variant='contained'
+                        color='success'
+                    >
+                        <Search/>
+                        Search
+                    </Button>
+                </div>
+            </Grid>
+        </Grid>
         <GoogleMap/>
 
     </div>
