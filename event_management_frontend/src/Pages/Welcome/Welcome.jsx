d994cd8b739b82872919ad673b1439589e465087
Sanjana_Nalajitha <snalajitha@gmail.com>
2024-09-10 16:07:16 +0530

    Welcome page updated

--- event_management_frontend/src/Pages/Welcome/Welcome.jsx
+++ event_management_frontend/src/Pages/Welcome/Welcome.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Box, Container, Typography } from '@mui/material';
 import logo from '../../Assets/logo12.png'; 
 import './Welcome.css'; 
 
 const Welcome = () => {
+
+    const images = [
+        require('../../Assets/background.jpg.webp'),
+        require('../../Assets/background2.jpg'),
+        require('../../Assets/background3.jpg'),
+      ];
+    
+      const [bgIndex, setBgIndex] = useState(0);
+    
+      useEffect(() => {
+        const interval = setInterval(() => {
+          setBgIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 5000); 
+        return () => clearInterval(interval); 
+      }, [images.length]);
+
   return (
-      <Box className="welcome-page">
+      <Box className="welcome-page"
+          style={{
+              backgroundImage: `url(${images[bgIndex]})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              transition: 'background-image 1s ease-in-out', // Smooth transition
+          }}
+      >
           <Container maxWidth="sm" style={{ textAlign: 'center' }}>
               <img src={logo} alt="Website Logo" className="logo" />
               {/* <Typography variant="h2" gutterBottom style={{ color: '#fff', marginTop: '20px' }}>
@@ -15,11 +38,11 @@ const Welcome = () => {
                   Please sign in or sign up to continue
               </Typography>
 
-              <Button variant="contained" color="primary" size="large" style={{ marginRight: '10px' }}>
+              <Button variant="outlined" color="primary" size="large" style={{ marginRight: '10px' }}>
                   Sign In
               </Button>
 
-              <Button variant="contained" color="secondary" size="large">
+              <Button variant="outlined" color="secondary" size="large">
                   Sign Up
               </Button>
           </Container>
